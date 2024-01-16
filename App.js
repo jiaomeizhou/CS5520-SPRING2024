5cf8bb77b442311aacee592c74f77895b18686da
jiaomeizhou <zhoujiaomei@outlook.com>
2024-01-16 14:02:37 -0800

    add header

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hearder from './components/Header'
 
 export default function App() {
-  const appName = "My First React Native App";
+  const appName = "My Native App";
   return (
     <View style={styles.container}>
       <Text>welcome to {appName} </Text>
       <StatusBar style="auto" />
+      <Hearder name="My Native App" version={2}/>
     </View>
   );
 }
