0f6d7df282435a5e52c41baa0d89f1af7d069e60
jiaomeizhou <zhoujiaomei@outlook.com>
2024-01-23 14:30:51 -0800

    add goal icon in two ways

--- components/Input.js
+++ components/Input.js
@@ -1,35 +1,37 @@
-import { Button, StyleSheet, Text, TextInput, View, Modal } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View, Modal, Image } from "react-native";
 import React, { useState } from "react";
 
-export default function Input({inputHandler, modelVisible, dismissModal}) {
-  const [text, setText] = useState("");
-  // callback handler
-  function changeTextHandler(changedText) {
-    console.log("user is typing ", changedText);
-    setText(changedText);
-  }
-  function confirmFunction() {
-    inputHandler(text);
-  }
-  function cancelHandler() {
-    // hide the modal
-    dismissModal();
-  }
-  return (
-    <Modal visible={modelVisible}>
-    <View style={styles.container}>
-      <TextInput
-        placeholder="Type something"
-        style={styles.input}
-        value={text}
-        onChangeText={changeTextHandler}
-      />
-      <Button title='Confirm' onPress={confirmFunction} />
-      <Button title="Cancel" onPress={cancelHandler}/>
-    </View>
-    </Modal>
+export default function Input({ inputHandler, modelVisible, dismissModal }) {
+    const [text, setText] = useState("");
+    // callback handler
+    function changeTextHandler(changedText) {
+        console.log("user is typing ", changedText);
+        setText(changedText);
+    }
+    function confirmFunction() {
+        inputHandler(text);
+    }
+    function cancelHandler() {
+        // hide the modal
+        dismissModal();
+    }
+    return (
+        <Modal visible={modelVisible}>
+            <View style={styles.container}>
+                <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/2617/2617812.png' }} style={styles.image} />
+                <Image source={require('../assets/goal.png')} style={styles.image} />
+                <TextInput
+                    placeholder="Type something"
+                    style={styles.input}
+                    value={text}
+                    onChangeText={changeTextHandler}
+                />
+                <Button title='Confirm' onPress={confirmFunction} />
+                <Button title="Cancel" onPress={cancelHandler} />
+            </View>
+        </Modal>
 
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -44,4 +46,8 @@ const styles = StyleSheet.create({
         borderBottomColor: "purple",
         width: "50%",
     },
+    image: {
+        width: 50,
+        height: 50,
+    }
 });
