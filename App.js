b85a92d5456921cfcb6c8d6cb9b84b8aa7dbdcea
jiaomeizhou <zhoujiaomei@outlook.com>
2024-02-06 14:49:35 -0800

    update dynamic title on the goal details page

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Home from './components/Home'
 import { NavigationContainer } from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import GoalDetails from './components/GoalDetails'
 
 const Stack = createNativeStackNavigator()
@@ -10,9 +10,16 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={GoalDetails} />
+      <Stack.Navigator screenOptions={{
+        headerStyle: { backgroundColor: 'darkmagenta' },
+        headerTintColor: 'white'
+      }}>
+        <Stack.Screen name="Home" component={Home} options={{
+          headerTitle: 'All my goals',
+        }} />
+        <Stack.Screen name="Details" component={GoalDetails} options={
+          ({ route }) => ({ headerTitle: route.params.goal.text, headerTitleAlign: 'center' })
+        }/>
       </Stack.Navigator>
     </NavigationContainer>
   )
