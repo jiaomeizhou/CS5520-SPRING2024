82a52bee779076016b84259cae58aa8fea01e1d8
jiaomeizhou <zhoujiaomei@outlook.com>
2024-02-06 13:58:07 -0800

    add native stack navigation

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Home from './components/Home'
+import { NavigationContainer } from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
