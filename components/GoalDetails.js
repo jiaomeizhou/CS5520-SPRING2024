4c86dbf96dc3522c71765ec974d06637797f8ef4
jiaomeizhou <zhoujiaomei@outlook.com>
2024-03-26 14:46:05 -0700

    render map image and locate me

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,12 +1,26 @@
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Image} from "react-native";
 import React, { useEffect, useState } from "react";
 import GoalUsers from "./GoalUsers";
-import { storage } from "../firebase-files/firebaseSetup";
 import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../firebase-files/firebaseSetup";
 
 export default function GoalDetails({ navigation, route }) {
   const [warning, setWatning] = useState(false);
   const [imageURL, setImageURL] = useState("");
+
+  useEffect(() => {
+    async function getImageURL() {
+      if (route.params.data) {
+        const imageUri = route.params.data.imageUri;
+        const imageRef = ref(storage, imageUri);
+        const imageDownloadURL = await getDownloadURL(imageRef);
+        setImageURL(imageDownloadURL);
+      }
+    }
+    getImageURL();
+  }, []);
+
+
   function warningHandler() {
     console.log("warning");
     setWatning(true);
@@ -20,17 +34,6 @@ export default function GoalDetails({ navigation, route }) {
     });
   }, []);
 
-  useEffect(() => {
-    async function getImageURL() {
-      if (route.params.data) {
-        const imageUri = route.params.data.imageUri;
-        const imageRef = ref(storage, imageUri);
-        const imageDownloadURL = await getDownloadURL(imageRef);
-        setImageURL(imageDownloadURL);
-      }
-    }
-    getImageURL();
-  }, []);
   return (
     <View>
       {route.params ? (
@@ -41,27 +44,15 @@ export default function GoalDetails({ navigation, route }) {
       ) : (
         <Text> "Extra details"</Text>
       )}
-      {imageURL && (
-        <Image
-          style={styles.image}
-          source={{
-            uri: imageURL,
-          }}
-        />
-      )}
       {warning && <Text style={{ color: "red" }}>WARNING</Text>}
       <Button
         title="extra details"
         onPress={() => navigation.push("Details")}
       />
       <GoalUsers id={route.params.data.id} />
+      <Image source={{ uri: imageURL }} style={{ width: 200, height: 200 }} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  image: {
-    width: 100,
-    height: 100,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/LocationManager.js
@@ -0,0 +1,54 @@
+import { View, Text, Button, Image } from 'react-native'
+import React, { useState } from 'react'
+import * as Location from 'expo-location'
+
+export default function LocationManager() {
+    const [status, requestPermission] = Location.useForegroundPermissions();
+    const [location, setLocation] = useState(null);
+
+    async function verifyPemission() {
+        if (status.granted) {
+            return true;
+        }
+        try {
+            const result = await requestPermission();
+            return result.granted;
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    }
+
+    const locateUserHandler = async () => {
+        try {
+            const havePermission = await verifyPemission();
+            console.log("have permission? ", havePermission);
+            if (!havePermission) {
+                alert('Permission to access location is required');
+                return;
+            }
+            const location = await Location.getCurrentPositionAsync();
+            console.log(location.coords.latitude, location.coords.longitude);
+            console.log("location: ", location);
+            setLocation(
+                {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude
+                }
+            );
+
+        }
+        catch (error) {
+            console.log("error: ", error);
+        }
+    }
+
+    return (
+        <View>
+            <Button title="Locate me" onPress={locateUserHandler} />
+            {location && <Image source={{
+                uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude} & zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=AIzaSyC9e8wMt9caQ4-A7GwyYWuaEgzqvFQuk24`
+            }} style={{ width: 200, height: 200 }} />
+            }
+        </View>
+    )
+}
